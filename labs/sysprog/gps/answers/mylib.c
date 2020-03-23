1f542f077eb9cfab8e7700c70818e82ffe211aa8
HamidHacene <hamid.hacene@ensta-bretagne.org>
2020-03-23 10:42:16 +0100

    answers_q4&q5

---
+++ labs/sysprog/gps/answers/mylib.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+int add(int a, int b)
+{
+	return a+b;
+
+}
+
+void bjr(int n)
+{
+	for(int i=0; i<n; i++)
+	{
+		printf("Bonjour\n");
+	}
+}
