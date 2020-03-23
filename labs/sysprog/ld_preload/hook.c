94584b3b4ee165a80829149b7816c2c5d46d049b
Bertrand T <bertrand.turck@ensta-bretagne.org>
2020-03-23 14:58:54 +0100

    1

--- labs/sysprog/ld_preload/hook.c
+++ labs/sysprog/ld_preload/hook.c
@@ -1,4 +1,25 @@
-// Question 8 : write here the buggy function without errors
 
+#include <stdio.h>
+
+#define KNOT_TO_KMH 1.852
+
+int iteration = 0;
+
+// Question 8 : write here the buggy function without errors
+int knot_to_kmh_str(float not, size_t size, char * format, char * kmh_str)
+{
+    float kmh = KNOT_TO_KMH * not;
+    snprintf(kmh_str, size, format, kmh);
+
+#ifndef GPS_OK
+    iteration++;
+    if (iteration == 2)
+    {
+        puts(NULL);
+    }
+#endif
+
+    return (float)kmh;
+}
 // Question 12 : write printf with a signal handler
 
--- labs/sysprog/ld_preload/run.sh
+++ labs/sysprog/ld_preload/run.sh
@@ -1,7 +1,10 @@
 #! /bin/sh
 
 SCRIPT=`readlink -f $0`
-ROOT_DIR=`dirname $SCRIPT`/../../../gps
+ROOT_DIR=`dirname $SCRIPT`/../gps
 
 export LD_LIBRARY_PATH=$ROOT_DIR/lib
+export LD_PRELOAD=libhook.so
+
+
 $ROOT_DIR/bin/gps
