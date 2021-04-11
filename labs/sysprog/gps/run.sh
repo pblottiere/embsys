5e94a06eef4af8b5b8e20054dbd178e3f10d6863
ngnepist <stephane.ngnepiepaye@ensta-bretagne.org>
2021-04-11 17:18:16 +0200

    TP2 Exercice1 Exercice2

--- labs/sysprog/gps/run.sh
+++ labs/sysprog/gps/run.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
-
 SCRIPT=`readlink -f $0`
+echo $SCRIPT
 ROOT_DIR=`dirname $SCRIPT`
-
+echo $ROOT_DIR
 export LD_LIBRARY_PATH=$ROOT_DIR/lib
-$ROOT_DIR/bin/gps
+$ROOT_DIR/bin/gps 
--- labs/sysprog/ld_preload/hook.c
+++ labs/sysprog/ld_preload/hook.c
@@ -1,4 +1,17 @@
 // Question 8 : write here the buggy function without errors
+//-----------------------------------------------------------------------------
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+int write_vtg(int fd)
+{
+return 0;
+}
 
 // Question 12 : write printf with a signal handler
 
