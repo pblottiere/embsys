697fe652a4f87a0c280e23c8f36328a51cb59970
Katell-Lag <katell.lagattu@ensta-bretagne.org>
2021-04-12 11:15:03 +0200

    TP autotools premier commit

--- labs/sysprog/ld_preload/hook.c
+++ labs/sysprog/ld_preload/hook.c
@@ -1,4 +1,25 @@
 // Question 8 : write here the buggy function without errors
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <stdlib.h>
 
+
+int knot_to_kmh_str(float not, size_t size, char * format, char * kmh_str)
+{
+    float kmh = KNOT_TO_KMH * not;
+    snprintf(kmh_str, size, format, kmh);
+
+#ifndef GPS_OK
+    iteration++;
+    if (iteration == 2)
+    {
+        puts("ok");
+    }
+#endif
+
+    return kmh;
+}
 // Question 12 : write printf with a signal handler
 
